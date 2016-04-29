2a4f4484101fa4c7ecc88caa10fe4613340d12f0
Ionut Pistol <ipistol@adobe.com>
2016-04-29 20:01:51 +0300

    Implemented filter and sort in table

--- app/mainController.js
+++ app/mainController.js
@@ -2,6 +2,10 @@
 
 angular.module('founders')
   .controller('mainController', ['$scope', '$http', function($scope, $http) {
+    $scope.sortField = 'id';
+    $scope.sortReverse  = false;
+    $scope.search  = '';
+
     $http.get('founders.json').success(function(data) {
       $scope.founders = data;
     });

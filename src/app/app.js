2b563f89f0bbd8c9de3a79c92fc53264af9b09c1
Ionut Pistol <ionut.pistol@gmail.com>
2016-05-27 20:26:49 +0300

    The content is fluid, responsive

--- src/app/app.js
+++ src/app/app.js
@@ -8,14 +8,7 @@ angular.module('founders', ['ui.router', 'ngFileUpload', 'ui.bootstrap'])
       $stateProvider
         .state('home', {
           url:'/',
-          templateUrl: 'app/main/main2.html',
+          templateUrl: 'app/main/main.html',
           controller: 'mainController'
-        })
-        .state('fluid', {
-          url:'/fluid',
-          templateUrl: 'app/main/fluid.html'
         });
-
-      $httpProvider.defaults.useXDomain = true;
-      delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }]);
